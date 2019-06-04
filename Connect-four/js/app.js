2aa1847baee34379c9ec07db0ebb150c80560323
Trevor <tfattig88@gmail.com>
2019-06-04 10:21:22 -0400

    Add files via upload

---
+++ Connect-four/js/app.js
@@ -0,0 +1,22 @@
+const game = new Game();
+
+/** 
+ * Listens for click on `#begin-game` and calls startGame() on game object
+ */
+document.getElementById('begin-game').addEventListener('click', function(){
+    game.startGame();
+    this.style.display = 'none';
+    document.getElementById('play-area').style.opacity = '1';
+    document.getElementById('start-over').style.display = 'block';
+});
+
+$('#start-over').on('click', function(){
+    location.reload();
+});
+
+/** 
+ * Listen for keyboard presses
+ */
+document.addEventListener('keydown', function(event){
+    game.handleKeydown(event);
+});
